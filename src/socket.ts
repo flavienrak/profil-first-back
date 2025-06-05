91a5876b15667263109753fd60cafff49cbacfcf
flavienrak <flavien.andrisoarak@gmail.com>
2025-06-05 05:44:57 +0300

    fix(json): add limit 10mb

--- src/socket.ts
+++ src/socket.ts
@@ -28,9 +28,9 @@ app.use(
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   }),
 );
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(compression());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 
 const openai = new OpenAI({
